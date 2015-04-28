44a1db647df057ffb42ea13f7aab7e39b3a36dca
fractalconsulting <sid@kabadiwallaconnect.in>
2015-04-28 11:22:07 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$('.banner').height($(window).height());
+		$('.homebanner').height($(window).height());
+		
+		$(function() {
+			console.log( "ready!" );
+			$('.homecontent').css({'padding-top':$(window).height()})
+			var elementPosition= $('.homecontent').find('.lead').offset().top-100;
+			console.log(elementPosition);
+			$(window).scroll(function(){
+				console.log($(window).scrollTop());
+				if($(window).scrollTop() > elementPosition){
+					$('#navbarid').css({'background':'rgba(0,0,0,0.5)','transition':'1s'});
+				} else {
+					$('#navbarid').css({'background':'none'})
+				}    
+			});
+			
+			$('.navbar-nav').find('a').on('click', function(e){
+				e.preventDefault();
+				var offset=$($(this).attr('href')).offset().top;
+				$('html,body').animate({
+					scrollTop: offset
+				}, 1000, "linear");
+			});
+	});
